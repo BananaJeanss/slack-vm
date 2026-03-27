f2a1953c4e401b37b9fec0277efb8d9515b0b4a0
BananaJeans <18456310+BananaJeanss@users.noreply.github.com>
2026-03-27 12:09:28 +0200

    add typeenter

---
+++ src/commands/typeenter.ts
@@ -0,0 +1,52 @@
+import { sleep } from "bun";
+import { keySequence } from "../keypress";
+import { getQemuKey } from "../keymap";
+import printScreen from "../print";
+import fs from "fs";
+import { prefixUserid } from "../commandtools";
+import type { CommandHandler } from "../types";
+
+// just type.ts except it presses enter at the end
+export default {
+  trigger: ["typeenter", "typepress"],
+  handler: (async (args, say, msg, app) => {
+    const rawText = args.join(" ");
+    if (!rawText) {
+      await say(`Please provide text to type. Usage: \`type <text>\``);
+      return;
+    }
+    const input = rawText
+    let allValid = true;
+    // reasonable limit
+    if (input.length > 256) {
+      await say(`Input too long. Max 256 characters.`);
+      return;
+    }
+    for (const char of input) {
+      const qemuKey = getQemuKey(char);
+      if (qemuKey === null) {
+        allValid = false;
+        await say(`Character "${char}" not recognized.`);
+        break;
+      }
+    }
+    if (allValid) {
+      await keySequence(input.split(""));
+      await sleep(25);
+      await keySequence(["enter"]);
+
+      // delay to let the OS react before screenshotting
+      await sleep(1000);
+
+      const screenshot = await printScreen();
+      if (screenshot) {
+        await app.client.files.uploadV2({
+          channel_id: msg.channel,
+          file: fs.createReadStream(screenshot),
+          filename: screenshot,
+          title: prefixUserid(`Typed "${input}"`, msg),
+        });
+      }
+    }
+  }) as CommandHandler,
+};
